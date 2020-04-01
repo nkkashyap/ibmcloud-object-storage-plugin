aaf695dfbcf561b84b850de3e7dce35382e0435b
Neeraj K Kashyap <nkashyap@in.ibm.com>
2020-04-01 21:37:48 +0530

    fixed the endpoint format for auto COS Service discovery

--- driver/driver.go
+++ driver/driver.go
@@ -466,12 +466,12 @@ func (p *S3fsPlugin) mountInternal(mountRequest interfaces.FlexVolumeMountReques
 	}
 	if options.CAbundleB64 != "" {
 		CaBundleKey, err := parser.DecodeBase64(options.CAbundleB64)
-		caFileName := "_ ca.crt"
+		caFileName := "_ca.crt"
 		if options.CosServiceIP != "" {
-			caFileName = options.CosServiceIP + "_ ca.crt"
+			caFileName = options.CosServiceIP + "_ca.crt"
 		} else {
 			pvName := filepath.Base(mountRequest.MountDir)
-			caFileName = pvName + "_ ca.crt"
+			caFileName = pvName + "_ca.crt"
 		}
 		caFile := path.Join(caPath, caFileName)
 		p.Logger.Info(podUID+": CA CERT", zap.String("CA bundle file", caFile))
